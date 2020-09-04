5cc7d54b734dd0fc99e52822d13b7b58cc2bd61e
pratikdaigavane <daigavanep@gmail.com>
2020-09-04 23:20:51 +0530

    docker init

---
+++ main-server/app.js
@@ -0,0 +1,24 @@
+require('dotenv').config({path: `.env.${process.env.NODE_ENV}`})
+
+const express = require('express')
+const pino = require('pino');
+const expressPino = require('express-pino-logger');
+
+const logger = pino({
+    level: process.env.LOG_LEVEL || 'info',
+    prettifier: require('pino-colada')
+});
+
+const expressLogger = expressPino({logger});
+const port = parseInt(process.env.port)
+const app = express()
+
+app.use(expressLogger);
+
+app.get('/', (req, res) => {
+    res.status(200).send({ error: 'something blew up' })
+})
+
+app.listen(port, () => {
+    logger.info(`Listening at http://localhost:${port}`)
+})
