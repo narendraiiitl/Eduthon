f8e1e8feaa71fb33b6a927c886c1fcf0d00aa978
AVC0706 <atharvvijay4@gmail.com>
2020-09-05 01:43:05 +0530

    Dark mode Added

--- main-client/src/App.jsx
+++ main-client/src/App.jsx
@@ -1,14 +1,52 @@
 import React from 'react';
-import {Route, Switch, withRouter} from "react-router-dom";
+import {Route, Switch,} from "react-router-dom";
+import {useThemeSwitcher} from "react-css-theme-switcher";
 import SignIn from "./pages/sign-in/signIn.page"
 import './App.less'
+import DarkModeToggle from "react-dark-mode-toggle";
+import {Space} from "antd";
+import {reactLocalStorage} from 'reactjs-localstorage';
 
-const App = () => (
-    <Switch>
-        <Route path='/login'>
-            <SignIn/>
-        </Route>
-    </Switch>
-);
+
+const App = () => {
+    const {switcher, status, themes} = useThemeSwitcher();
+
+    const [isDarkMode, setIsDarkMode] = React.useState( async ()=>{
+         const dark = await reactLocalStorage.get('theme', 'light') !== 'light';
+        await switcher({theme: dark ? themes.dark : themes.light});
+        return dark
+    })
+
+    const toggleTheme = (isChecked) => {
+        setIsDarkMode(isChecked);
+        reactLocalStorage.set('theme',isChecked?'dark':'light')
+        switcher({theme: isChecked ? themes.dark : themes.light});
+    };
+
+    // Avoid theme change flicker
+    if (status === "loading") {
+        return null;
+    }
+    return (
+        <>
+
+            <Space style={{float: "right", padding: "10px 30px"}}>
+                <DarkModeToggle
+                    onChange={toggleTheme}
+                    checked={isDarkMode}
+                    size={80}
+                />
+            </Space>
+
+
+            <Switch>
+                <Route path='/login'>
+                    <SignIn/>
+                </Route>
+            </Switch>
+
+        </>
+    );
+}
 
 export default App;
--- main-client/src/index.js
+++ main-client/src/index.js
@@ -4,11 +4,18 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from "react-router-dom";
+import { ThemeSwitcherProvider } from "react-css-theme-switcher";
 
+const themes = {
+    dark: `${process.env.PUBLIC_URL}/dark-theme.css`,
+    light: `${process.env.PUBLIC_URL}/light-theme.css`,
+};
 ReactDOM.render(
     <React.StrictMode>
         <BrowserRouter>
-            <App />
+            <ThemeSwitcherProvider themeMap={themes} defaultTheme="light">
+                <App />
+            </ThemeSwitcherProvider>
         </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
