9471cbaf7174676e74902aa68e8b34cf0745ba4b
Pratik Daigavane <39241098+pratikdaigavane@users.noreply.github.com>
2020-09-06 19:10:12 +0530

    Update terminal.component.jsx

--- main-client/src/components/terminalComponent/terminal.component.jsx
+++ main-client/src/components/terminalComponent/terminal.component.jsx
@@ -12,8 +12,8 @@ export default class TerminalComponent extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            ws : `wss://${this.props.roomUrl}`,
-            http : `https://${this.props.roomUrl}`
+            ws : `ws://${this.props.roomUrl}`,
+            http : `http://${this.props.roomUrl}`
         }
 
         this.xtermRef = React.createRef();
